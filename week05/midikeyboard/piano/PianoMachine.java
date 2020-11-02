d12993e25f3ce9af3c8f7fee21598594b0dec6aa
James R T <jamestiotio@gmail.com>
2020-11-03 03:02:57 +0800

    Add Problem Set 2B answers code

---
+++ week05/midikeyboard/piano/PianoMachine.java
@@ -0,0 +1,238 @@
+package piano;
+
+import javax.sound.midi.MidiUnavailableException;
+
+import midi.Midi;
+import music.Pitch;
+import midi.Instrument;
+import music.NoteEvent;
+import java.util.ArrayDeque;
+import java.util.LinkedHashSet;
+import java.util.Iterator;
+
+public class PianoMachine {
+
+    private Midi midi;
+    private Instrument currentInstrument = Midi.DEFAULT_INSTRUMENT;
+    private int semitoneShift = 0;
+    private boolean isRecording = false;
+    private LinkedHashSet<Pitch> currentlyPlayingNotes = new LinkedHashSet<>();
+    private ArrayDeque<NoteEvent> recordingSequence = new ArrayDeque<>();
+
+    /**
+     * constructor for PianoMachine.
+     * 
+     * initialize midi device and any other state that we're storing.
+     */
+    public PianoMachine() {
+        try {
+            midi = Midi.getInstance();
+        }
+
+        catch (MidiUnavailableException e1) {
+            System.err.println("Could not initialize midi device");
+            e1.printStackTrace();
+            return;
+        }
+    }
+
+    /**
+     * Begins playing a note if the note is not already playing. If isRecording is turned on, add a
+     * start NoteEvent to the recordingSequence.
+     * 
+     * @param rawPitch - the note to be played
+     */
+    public void beginNote(Pitch rawPitch) {
+        Pitch actualPitch = rawPitch.transpose(this.semitoneShift);
+
+        if (!this.currentlyPlayingNotes.contains(actualPitch)) {
+            this.midi.beginNote(actualPitch.toMidiFrequency(), this.currentInstrument);
+            this.currentlyPlayingNotes.add(actualPitch);
+        }
+
+        if (this.isRecording) {
+            this.recordingSequence.addLast(new NoteEvent(actualPitch, System.currentTimeMillis(),
+                    this.currentInstrument, NoteEvent.Kind.start));
+        }
+    }
+
+    /**
+     * Stops playing a note if the note is not already playing. If isRecording is turned on, add a
+     * stop NoteEvent to the recordingSequence.
+     * 
+     * @param rawPitch - the note to be stopped
+     */
+    public void endNote(Pitch rawPitch) {
+        Pitch actualPitch = rawPitch.transpose(this.semitoneShift);
+
+        if (this.currentlyPlayingNotes.contains(actualPitch)) {
+            this.midi.endNote(actualPitch.toMidiFrequency(), this.currentInstrument);
+            this.currentlyPlayingNotes.remove(actualPitch);
+        }
+
+        if (this.isRecording) {
+            this.recordingSequence.addLast(new NoteEvent(actualPitch, System.currentTimeMillis(),
+                    this.currentInstrument, NoteEvent.Kind.stop));
+        }
+    }
+
+    /**
+     * Changes to the next instrument in the standard ordering of the Instrument enum list (or the
+     * first in the ordering if the current instrument is the last). If any notes are currently
+     * being played on the current instrument, those notes will be stopped and restarted on the new
+     * instrument.
+     * 
+     * @modifies this.currentInstrument
+     */
+    public void changeInstrument() {
+        LinkedHashSet<Pitch> temp = new LinkedHashSet<>(this.currentlyPlayingNotes);
+
+        if (!this.currentlyPlayingNotes.isEmpty()) {
+            Iterator<Pitch> endIter = temp.iterator();
+
+            while (endIter.hasNext()) {
+                this.endNote(endIter.next());
+            }
+        }
+
+        this.currentInstrument = this.currentInstrument.next();
+
+        if (!temp.isEmpty()) {
+            Iterator<Pitch> beginIter = temp.iterator();
+
+            while (beginIter.hasNext()) {
+                this.beginNote(beginIter.next());
+            }
+        }
+    }
+
+    /**
+     * Shifts the notes that the keys play by one octave (12 semitones) up. The maximum final shift
+     * is two octaves up from the starting pitches. Also shifts all currently playing notes to the
+     * new octave.
+     * 
+     * @modifies this.semitoneShift
+     */
+    public void shiftUp() {
+        if (this.semitoneShift < 24) {
+            LinkedHashSet<Pitch> temp = new LinkedHashSet<>(this.currentlyPlayingNotes);
+
+            if (!this.currentlyPlayingNotes.isEmpty()) {
+                Iterator<Pitch> endIter = temp.iterator();
+
+                while (endIter.hasNext()) {
+                    this.endNote(endIter.next());
+                }
+            }
+
+            this.semitoneShift += Pitch.OCTAVE;
+
+            if (!temp.isEmpty()) {
+                Iterator<Pitch> beginIter = temp.iterator();
+
+                while (beginIter.hasNext()) {
+                    this.beginNote(beginIter.next());
+                }
+            }
+        }
+    }
+
+    /**
+     * Shifts the notes that the keys play by one octave (12 semitones) down. The maximum final
+     * shift is two octaves down from the starting pitches. Also shifts all currently playing notes
+     * to the new octave.
+     * 
+     * @modifies this.semitoneShift
+     */
+    public void shiftDown() {
+        if (this.semitoneShift > -24) {
+            LinkedHashSet<Pitch> temp = new LinkedHashSet<>(this.currentlyPlayingNotes);
+
+            if (!this.currentlyPlayingNotes.isEmpty()) {
+                Iterator<Pitch> endIter = temp.iterator();
+
+                while (endIter.hasNext()) {
+                    this.endNote(endIter.next());
+                }
+            }
+
+            this.semitoneShift -= Pitch.OCTAVE;
+
+            if (!temp.isEmpty()) {
+                Iterator<Pitch> beginIter = temp.iterator();
+
+                while (beginIter.hasNext()) {
+                    this.beginNote(beginIter.next());
+                }
+            }
+        }
+    }
+
+    /**
+     * Start/stop recording the notes played. Toggles the current isRecording state, unless it's in
+     * playback mode.
+     * 
+     * @modifies this.isRecording
+     * @return this.isRecording - True if recording is turned on when method returns, false
+     *         otherwise.
+     */
+    public boolean toggleRecording() {
+        this.isRecording = !this.isRecording;
+
+        if (this.isRecording) {
+            this.recordingSequence.clear();
+        }
+
+        return this.isRecording;
+    }
+
+    /**
+     * Plays back each note stored in recordingSequence. If nothing is stored, nothing is played. A
+     * playback cannot be executed if a recording is currently taking place. If the note is not the
+     * last in the sequence, put a rest time right after it. This method is considered to be atomic.
+     * Temporarily pauses all the current notes being played in the current instrument.
+     */
+    public void playback() {
+        if (!this.isRecording && !this.recordingSequence.isEmpty()) {
+            LinkedHashSet<Pitch> temp = new LinkedHashSet<>(this.currentlyPlayingNotes);
+
+            if (!this.currentlyPlayingNotes.isEmpty()) {
+                Iterator<Pitch> endIter = temp.iterator();
+
+                while (endIter.hasNext()) {
+                    this.endNote(endIter.next());
+                }
+            }
+
+            long timestamp = 0;
+            Iterator<NoteEvent> note = this.recordingSequence.iterator();
+
+            while (note.hasNext()) {
+                NoteEvent event = note.next();
+
+                if (timestamp > 0) {
+                    Midi.rest((int) Math.round((event.getTime() - timestamp) / 10.0));
+                }
+
+                timestamp = event.getTime();
+
+                if (event.getKind() == NoteEvent.Kind.start) {
+                    this.midi.beginNote(event.getPitch().toMidiFrequency(), event.getInstr());
+                }
+
+                else if (event.getKind() == NoteEvent.Kind.stop) {
+                    this.midi.endNote(event.getPitch().toMidiFrequency(), event.getInstr());
+                }
+            }
+
+            if (!temp.isEmpty()) {
+                Iterator<Pitch> beginIter = temp.iterator();
+
+                while (beginIter.hasNext()) {
+                    this.beginNote(beginIter.next());
+                }
+            }
+        }
+    }
+
+}
